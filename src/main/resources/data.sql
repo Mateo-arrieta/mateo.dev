67d0490d8234dd6d06d01dbe88a7314c3da20481
mateo <mateo12arrieta.02@gmail.com>
2026-09-17 07:29:44 -0500

    estructura base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+json
+[
+  {
+    "id": 1,
+    "nombre": "Ana Pérez",
+    "email": "ana@mail.com",
+    "edad": 25
+  },
+  {
+    "id": 2,
+    "nombre": "Luis Gómez",
+    "email": "luis@mail.com",
+    "edad": 30
+  }
+]
